f1c1c292f1e13847da4d9ba47f6149753903c844
Eisyah <eisyahfarihah.dali@gmail.com>
2023-01-24 18:41:13 +0800

    calendar update

--- app/src/main/java/com/example/newgroup4/LecturerHome.java
+++ app/src/main/java/com/example/newgroup4/LecturerHome.java
@@ -1,17 +1,31 @@
 package com.example.newgroup4;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.newgroup4.adapter.CalendarAdapt;
 import com.example.newgroup4.model.SharedPrefManager;
 import com.example.newgroup4.model.User;
 
-public class LecturerHome extends AppCompatActivity {
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class LecturerHome extends AppCompatActivity implements CalendarAdapt.OnitemListener {
+
+    private TextView monthYearText;
+    private RecyclerView calRecyleView;
+    private LocalDate selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +55,95 @@ public class LecturerHome extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void initWidgets()
+    {
+        calRecyleView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+    }
+
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapt calendarAdapter = new CalendarAdapt(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calRecyleView.setLayoutManager(layoutManager);
+        calRecyleView.setAdapter(calendarAdapter);
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date)
+    {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            yearMonth = YearMonth.from(date);
+        }
+
+        int daysInMonth = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            daysInMonth = yearMonth.lengthOfMonth();
+        }
+
+        LocalDate firstOfMonth = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            firstOfMonth = selectedDate.withDayOfMonth(1);
+        }
+        int dayOfWeek = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+        }
+
+        for(int i = 1; i <= 42; i++)
+        {
+            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
+            {
+                daysInMonthArray.add("");
+            }
+            else
+            {
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return  daysInMonthArray;
+    }
+
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return date.format(formatter);
+        }
+        else
+            return null;
+    }
+
+    public void previousMonthAction(View view)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            selectedDate = selectedDate.minusMonths(1);
+        }
+        setMonthView();
+    }
+
+    public void nextMonthAction(View view)
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            selectedDate = selectedDate.plusMonths(1);
+        }
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if (!dayText.equals("")) {
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        }
     }
 }
