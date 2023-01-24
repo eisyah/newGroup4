ea239d3be6c68be245fb25b0549f04899a508561
adam <nuclearboumb@gmail.com>
2023-01-24 17:16:09 +0800

    changed some slight stuff

---
+++ app/src/main/java/com/example/newgroup4/StudentAddAppt.java
@@ -0,0 +1,14 @@
+package com.example.newgroup4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentAddAppt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_add_appt);
+    }
+}
