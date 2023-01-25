5320ef40adfa2ff9a004b0aa070176957fbc03d1
Amielia <amieliatasha26@gmail.com>
2023-01-25 13:25:16 +0800

    interface appointment

--- app/src/main/java/com/example/newgroup4/adapter/ApptAdapter.java
+++ app/src/main/java/com/example/newgroup4/adapter/ApptAdapter.java
@@ -1,4 +1,64 @@
 package com.example.newgroup4.adapter;
 
-public class ApptAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newgroup4.R;
+import com.example.newgroup4.model.Appointment;
+
+
+import java.util.List;
+
+public class ApptAdapter extends RecyclerView.Adapter<ApptAdapter.ViewHolder>{
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView tvLectName;
+        public TextView tvStatus;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvLectName = (TextView) itemView.findViewById(R.id.tvLectName);
+            tvStatus = (TextView) itemView.findViewById(R.id.tvStatus);
+
+        }
+
+    }
+
+    private List<Appointment> mListData;
+    private Context mContext;
+
+    public ApptAdapter(Context context, List<Appointment> listData){
+        mListData = listData;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.appt_list_item, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        Appointment m = mListData.get(position);
+        holder.tvLectName.setText(m.getLectID());
+        holder.tvStatus.setText(m.getStatus());
+    }
+
+    @Override
+    public int getItemCount(){
+        return mListData.size();
+    }
 }
---
+++ app/src/main/java/com/example/newgroup4/remote/ApptService.java
@@ -0,0 +1,4 @@
+package com.example.newgroup4.remote;
+
+public interface ApptService {
+}
