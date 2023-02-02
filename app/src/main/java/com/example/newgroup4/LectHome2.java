333072a2e1737fcf7313aebd6bdc9def926c0f75
Eisyah <eisyahfarihah.dali@gmail.com>
2023-02-02 23:26:01 +0800

    lectHome 2

---
+++ app/src/main/java/com/example/newgroup4/LectHome2.java
@@ -0,0 +1,14 @@
+package com.example.newgroup4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LectHome2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lect_home2);
+    }
+}
